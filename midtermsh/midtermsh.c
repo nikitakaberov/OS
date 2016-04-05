f2f2982956135a402531278d9aebc2c030c4f352
nikita <nikita@nikita.(none)>
2016-04-05 21:23:14 +0300

    Fix bags

--- midtermsh/midtermsh.c
+++ midtermsh/midtermsh.c
@@ -25,29 +25,32 @@ int main()
   while(42) 
   {
     print$();
-    int cnt = 0;
     signal(SIGINT, sig_handler);
     char *progs[4096];
     int k = 0;
-    int flag = 0;
     char com[4096];
     char arg[4096];
     int first[2];
     int second[2];
     memset(com, 0, sizeof(char) * 4096);
     ssize_t len = read(STDIN_FILENO, com, 4096);
-    char* chs = strtok(com, " ");
+    com[len - 1] = 0;
+    char* chs = strtok(com, "|");
     while (chs != NULL)
     {
       progs[k++] = chs;
-      chs = strtok(NULL, " ");
+      chs = strtok(NULL, "|");
     }
 
+    int childs[4096];
+    int cld_cnt = 0;
     int i = 0;
     for (; i < k; i++)
     {
+    
       char * args[4096];
       int t = 0;
+      memset(args, 0, sizeof(args));
       char* chs = strtok(progs[i], " ");
       while (chs != NULL)
       {
@@ -57,43 +60,49 @@ int main()
       }
 
       args[t++] = NULL;
- execvp(args[0],  args); 
       pipe(second);
       pid_t child = fork();
-
-      if (!child)
+      if (child  < 0)
       {
-        if (cnt)
-          dup2(first[0], 0);
-        if (cnt)
-          close(first[0]);
-        if (cnt)
-          close(first[1]);
-        if (flag != 0)
-          dup2(second[1], 1);
-        close(second[0]);
-        close(second[1]);
-        //execvp(args[0],  args);    
+        perror("Failed\n");
       }
       else
       {
-        if (cnt)
+        childs[cld_cnt++] = child;
+      }
+      if (child == 0)
+      {
+        if (i > 0)
+        {
+          dup2(first[0], STDIN_FILENO);
           close(first[0]);
-        if (cnt)
           close(first[1]);
-        if (flag == 0)
-          close(second[0]);
-        if (flag == 0)
-          close(second[1]);
-      } 
+        }
+        if (i != k - 1)
+          dup2(second[1], STDOUT_FILENO);
+        close(second[0]);
+        close(second[1]);
+        execvp(args[0], args);
+      }
+      else
+      {
+        if (i > 0)
+        {
+          close(first[0]);
+          close(first[1]);
+        }
+        if (i == k - 1)
+        {
+        close(second[0]);
+        close(second[1]);
+        }
+      }
       first[0] = second[0];
       first[1] = second[1];
-
-      cnt++;    
-    }
-    pid_t wpid;
-    int status;
-    while ((wpid = wait(&status)) > 0) { }
+     }
+    for (i = 0; i < cld_cnt; i++)
+       waitpid(childs[i], 0, NULL);
+ 
   }
  
 }
